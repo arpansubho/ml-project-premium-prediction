e0d145bdb98c5d18f2e48e38d9b89e0fb719de17
Arpan Maity <arpansubho123@gmail.com>
2025-06-24 00:09:03 +0530

    Changed file path

--- prediction_helper.py
+++ prediction_helper.py
@@ -2,11 +2,11 @@ import joblib
 import pandas as pd
 from joblib import load
 
-model_rest = joblib.load(r"C:\Users\arpan.000\My project\1800 Tut\Projects\artifacts\model_rest.joblib")
-model_young = joblib.load(r"C:\Users\arpan.000\My project\1800 Tut\Projects\artifacts\model_young.joblib")
+model_rest = joblib.load("artifacts/model_rest.joblib")
+model_young = joblib.load("artifacts/model_young.joblib")
 
-scaler_rest = joblib.load(r"C:\Users\arpan.000\My project\1800 Tut\Projects\artifacts\scaler_rest.joblib")
-scaler_young = joblib.load(r"C:\Users\arpan.000\My project\1800 Tut\Projects\artifacts\scaler_young.joblib")
+scaler_rest = joblib.load("artifacts/scaler_rest.joblib")
+scaler_young = joblib.load("artifacts/scaler_young.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
